986e65abc648f32bb72e6fa313b8f04a3ca2714c
test1 <test@163.com>
2021-05-19 12:54:27 +0800

    first commit

---
+++ src/test/java/com/hsbc/pqddl/PqDdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.pqddl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqDdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
